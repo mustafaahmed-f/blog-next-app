d1e5a43871275a07fc3e86da1a2b4ec3674dcd6a
Mustafa Ahmed <mostafafikry97@gmail.com>
2025-08-12 18:31:01 +0300

    ✨ feat(docker): tried to apply docker and use it but hot reload didn't work so we will leave it for now

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "standalone",
+  // target: "serverless",
+  // webpackDevMiddleware: (config: any) => {
+  //   config.watchOptions = {
+  //     poll: 1000,
+  //     aggregateTimeout: 300,
+  //   };
+  //   return config;
+  // },
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+    <div className="font-sans grid bg-yellow-600 grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       welcome to our template
     </div>
   );
